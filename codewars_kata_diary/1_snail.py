d8d498ad60abf9e5032cb4db66d6e4f8f4f6fdc4
Ludwig Stumpp <42147848+LudwigStumpp@users.noreply.github.com>
2023-12-28 15:06:38 +0100

    Initial commit

---
+++ codewars_kata_diary/1_snail.py
@@ -0,0 +1,80 @@
+# https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/solutions/python
+
+from codewars_kata_diary import testing
+
+
+def snail(snail_map):
+    size_grid = len(snail_map)
+
+    if size_grid == 0 or size_grid == 1:
+        return snail_map[0]
+
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    visited = [[False for _ in range(size_grid)] for _ in range(size_grid)]
+
+    res = []
+
+    direction_idx = 0
+    loc_row = 0
+    loc_col = 0
+
+    while len(res) != size_grid**2:
+        # save current location
+        res.append(snail_map[loc_row][loc_col])
+        visited[loc_row][loc_col] = True
+
+        # check if need to rotate
+        loc_row_test = loc_row + directions[direction_idx % 4][0]
+        loc_col_test = loc_col + directions[direction_idx % 4][1]
+        is_out_of_square = (
+            loc_row_test < 0
+            or loc_row_test >= size_grid
+            or loc_col_test < 0
+            or loc_col_test >= size_grid
+        )
+        if is_out_of_square or visited[loc_row_test][loc_col_test]:
+            direction_idx += 1
+
+        # move forward
+        loc_row += directions[direction_idx % 4][0]
+        loc_col += directions[direction_idx % 4][1]
+
+    return res
+
+
+def snail2(snail_map):
+    res = []
+    while snail_map:
+        res += snail_map.pop(0)
+        snail_map = list(zip(*snail_map))[::-1]
+    return res
+
+
+def snail3(snail_map):
+    import numpy as np
+
+    res = []
+    array = np.array(snail_map)
+    while len(array) > 0:
+        res += array[0].tolist()
+        array = np.rot90(array[1:])
+    return res
+
+
+if __name__ == "__main__":
+    test_cases = [
+        [[]],
+        [[1]],
+        [[1, 2], [3, 4]],
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    ]
+
+    answers = [
+        [],
+        [1],
+        [1, 2, 4, 3],
+        [1, 2, 3, 6, 9, 8, 7, 4, 5],
+    ]
+
+    testing.test_func(test_cases, answers, snail)
---
+++ codewars_kata_diary/testing.py
@@ -0,0 +1,29 @@
+def test_func(inputs: list, expected: list, func: callable):
+    """Function to test a function with multiple test cases
+
+    Note: Yes I could have used pytest or unittest, but I want to keep this
+    repo simple.
+
+    Args:
+        inputs (list): List of inputs to test the function
+        expected (list): List of expected outputs
+        func (callable): Function to test
+    """
+    assert len(inputs) == len(
+        expected
+    ), "Length of inputs and expected should be equal"
+
+    print(f"Testing function {func.__name__}", end="\n\n")
+    num_passed = 0
+    for i, (inp, out) in enumerate(zip(inputs, expected)):
+        print(f"Test Case #{i + 1}")
+        print("Input: ", inp)
+        print("Expected Output: ", out)
+        res = func(inp)
+        if res == out:
+            print("[Passed]", end="\n\n")
+            num_passed += 1
+        else:
+            print("[Failed]", end="\n\n")
+
+    print(f"{num_passed}/{len(inputs)} test cases passed")
